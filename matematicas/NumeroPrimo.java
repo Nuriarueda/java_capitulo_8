98f1509399b0f99f9a446af5f3a530cd4346ed99
Nuriarueda <ruedareinanuria@gmail.com>
2023-01-26 19:43:16 +0100

    added

--- matematicas/NumeroPrimo.java
+++ matematicas/NumeroPrimo.java
@@ -2,15 +2,19 @@ package matematicas;
 
 
 /*Ejercicio de practica*/
+
+import java.util.Scanner;
 public class NumeroPrimo {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.print("Introduzca un número entero positivo: ");
-        int n = Integer.parseInt(System.console().readLine());
+        int n = sc.nextInt();
         if (esPrimo(n)) {
             System.out.println("El " + n + " es primo.");
         } else {
             System.out.println("El " + n + " no es primo.");
         }
+        sc.close();
     }
     public static boolean esPrimo(int x) {
         for (int i = 2; i < x; i++) {
