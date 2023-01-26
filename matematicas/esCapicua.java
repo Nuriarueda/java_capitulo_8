c003a35e90e00289680c6217c222f72d5b0f8834
Nuriarueda <ruedareinanuria@gmail.com>
2023-01-26 19:43:09 +0100

    added

---
+++ matematicas/esCapicua.java
@@ -0,0 +1,23 @@
+package matematicas;
+
+import java.util.Scanner;
+public class esCapicua {
+    public static void main (String []args) {
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Introduzca un número entero y te dire si es capicua o no: ");
+        int x = sc.nextInt();
+        if (esCapicua(x)) {
+            System.out.println("El " + x + " es capicua.");
+        } else {
+            System.out.println("El " + x + " no es capicua.");
+        }
+        sc.close();
+    }
+    public static boolean esCapicua(long x) {
+        return x == voltea(x);
+    }
+    public static boolean esCapicua(int x) {
+        return esCapicua((long)x);
+    }
+}
