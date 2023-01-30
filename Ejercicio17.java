57c00c23a180b230e0dcb6c18ea310983f83fd56
Nuriarueda <ruedareinanuria@gmail.com>
2023-01-30 16:47:34 +0100

    added

---
+++ Ejercicio17.java
@@ -0,0 +1,21 @@
+
+
+public class Ejercicio17 {
+    public static void main(String[] args) {
+        System.out.print("Introduzca un número entero positivo: ");
+        int n = Integer.parseInt(System.console().readLine());
+        if (esPrimo(n)) {
+            System.out.println("El " + n + " es primo.");
+        } else {
+            System.out.println("El " + n + " no es primo.");
+        }
+    }
+    public static boolean esPrimo(int x) {
+        for (int i = 2; i < x; i++) {
+            if ((x % i) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
